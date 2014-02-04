7cdfdefb6cac33d79207c63aa62fe6d09a191379
sneureiter <neureiter.stefan@gmail.com>
2014-02-04 20:30:06 +0100

    sdk

---
+++ sdk/android_sdk/SDK/src/com/wbtech/ums/objects/PostObjTouchEvent.java
@@ -0,0 +1,21 @@
+package com.wbtech.ums.objects;
+
+public class PostObjTouchEvent {
+	
+	
+	private String activity;
+	private String acc; //num
+	private String appkey;
+	private int xcoordiante;
+	private int ycoordiante;
+	private String object = "remove";
+	private String device_id;
+	private String session_id;
+	
+	public PostObjTouchEvent (String activity, String acc, String appkey, int xcoordinate, int ycoordinate, String object, String device_id, String session_id) {
+		super();
+		//if(event = null) 
+			
+	}
+
+}
